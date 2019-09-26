469a423e068272335fb6ccada3ff0cff224a85f5
matthieucham <matthieu.cham@gmail.com>
2019-09-26 23:52:30 +0200

    Validator and Recognizer for input expression

--- calc.py
+++ calc.py
@@ -2,8 +2,6 @@ import argparse
 
 # All valid chars of input expression
 ACCEPTED_CHARS = {
-    'digits': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-    'point': ['.'],
     'op_unary': ['-'],
     'op_binary': ['+', '-', '*', '/'],
     'par': ['(', ')']
@@ -16,6 +14,10 @@ class InvalidTokenError(Exception):
     pass
 
 
+class InvalidExpressionError(Exception):
+    pass
+
+
 def tokenize(expr):
     """
     Build a list of tokens (operators and operands) from an input expression string
@@ -24,26 +26,94 @@ def tokenize(expr):
     """
     if not expr:
         return []
-    delimiters = dict.copy(ACCEPTED_CHARS)  # a shallow copy is enough
-    delimiters.pop('digits')
-    delimiters.pop('point')
-    delimiters_set = {ch for val_list in delimiters.values() for ch in val_list}
     result_list = []
     start = 0
     for index, char in enumerate(expr):
-        if char in delimiters_set:
+        if char in VALID_CHARS_SET:
             curr_token = (expr[start:index]).strip()  # strip() to avoid pushing blank tokens
             if len(curr_token) > 0:
                 result_list.extend(curr_token.split())  # split() because of possible "inner" blank spaces
-                                                        # e.g "12 34" in expr. split() does the strip() also.
+                # e.g "12 34" in expr. split() does the strip() also.
             result_list.append(char)  # push operator as it's a token
             start = index + 1
     if start == 0:  # no delimiter found in expr
         return expr.split()
-    remainder_token = (expr[start:index+1]).strip()
+    remainder_token = (expr[start:index + 1]).strip()
     if len(remainder_token) > 0:
         result_list.extend(remainder_token.split())
     return result_list
+
+
+def is_value(token):
+    """
+    check if a token is a value, ie a stringified number
+    :param token: token to test
+    :return: True if value, false otherwise
+    """
+    if token is None:
+        return False
+    try:
+        float(token)
+        return True
+    except ValueError:
+        return False
+
+
+def validate_token(token):
+    if is_value(token) or token in VALID_CHARS_SET:
+        return token
+    raise InvalidTokenError(token)
+
+
+class Recognizer(object):
+    """
+    Implementation of  Recursive-descent recognition algorithm from http://www.engr.mun.ca/~theo/Misc/exp_parsing.htm
+    """
+
+    def __init__(self, tokexpr):
+        assert tokexpr is not None, "Expression to recognize cannot be None, should at least be []"
+        self.tokens = tokexpr
+        self.cursor = 0
+
+    def _next(self):
+        if self.cursor < len(self.tokens):
+            return self.tokens[self.cursor]
+        return None  # all tokens consumed
+
+    def _consume(self):
+        self.cursor += 1  # move cursor up
+
+    def _error(self):
+        raise InvalidExpressionError()
+
+    def _expect(self, token):
+        if self._next() == token:
+            self._consume()
+        else:
+            self._error()
+
+    def recognize(self):
+        self._e()
+        self._expect(None)
+
+    def _e(self):
+        self._p()
+        while self._next() in ACCEPTED_CHARS['op_binary']:
+            self._consume()
+            self._p()
+
+    def _p(self):
+        if is_value(self._next()):
+            self._consume()
+        elif self._next() == '(':
+            self._consume()
+            self._e()
+            self._expect(')')
+        elif self._next() in ACCEPTED_CHARS['op_unary']:
+            self._consume()
+            self._p()
+        else:
+            self._error()
 
 
 class ConcatArgs(argparse.Action):
@@ -63,4 +133,6 @@ if __name__ == "__main__":
     expression = parser.parse_args().expression
 
     tokens = tokenize(expression)
-    print(tokens)
+    for t in tokens:
+        validate_token(t)
+    Recognizer(tokens).recognize()
--- tests.py
+++ tests.py
@@ -38,3 +38,57 @@ class TokenizeTest(unittest.TestCase):
         self.assertEqual(['11.0', '+', '22.0', '*', '33.0', '/', '44.0', '-', '55.0'],
                          calc.tokenize('11.0+22.0*33.0/44.0-55.0'))
         self.assertEqual(['10', '+', '20', '*', '30', '/', '40', '-', '50'], calc.tokenize(' 10 + 20 * 30 / 40 - 50 '))
+
+
+class ValidateTokenTest(unittest.TestCase):
+    def test_none(self):
+        with self.assertRaises(calc.InvalidTokenError):
+            calc.validate_token(None)
+
+    def test_operators(self):
+        for op in "+-*/":
+            calc.validate_token(op)
+
+    def test_numbers(self):
+        calc.validate_token('0')
+        calc.validate_token('5')
+        calc.validate_token('5.5')
+        calc.validate_token('00.000')
+        calc.validate_token('10e2')
+        calc.validate_token('   45')
+
+    def test_pars(self):
+        calc.validate_token('(')
+        calc.validate_token(')')
+
+    def test_text(self):
+        with self.assertRaises(calc.InvalidTokenError):
+            calc.validate_token('a')
+        with self.assertRaises(calc.InvalidTokenError):
+            calc.validate_token('azerty')
+
+
+class RecognizerTest(unittest.TestCase):
+    def test_recognize_valid(self):
+        calc.Recognizer(['1']).recognize()
+        calc.Recognizer(['1.9']).recognize()
+        calc.Recognizer(['-', '1']).recognize()
+        calc.Recognizer(['-', '1.5']).recognize()
+        calc.Recognizer(['2', '+', '3']).recognize()
+        calc.Recognizer(['-', '2', '+', '3']).recognize()
+        calc.Recognizer(['2', '*', '3']).recognize()
+        calc.Recognizer(['2', '*', '(', '7', ')']).recognize()
+        calc.Recognizer(['2', '*', '(', '7', '-', '4', ')']).recognize()
+        calc.Recognizer(['(', '7', '-', '4', ')', '/', '2']).recognize()
+
+    def test_recognize_bad_grammar(self):
+        with self.assertRaises(calc.InvalidExpressionError):
+            calc.Recognizer(['1', '+']).recognize()
+
+    def test_recognize_bad_op(self):
+        with self.assertRaises(calc.InvalidExpressionError):
+            calc.Recognizer(['1', '#', '5']).recognize()
+
+    def test_recognize_bad_par(self):
+        with self.assertRaises(calc.InvalidExpressionError):
+            calc.Recognizer(['1', '/', '(', '2', '+', '3']).recognize()
