c096063a19eaf4cb9f5eafd1ffcb68322b93e206
Matthieu Grandrie <grandriem@mfi.fr>
2019-09-27 12:49:30 +0200

    Refactored with evaluable operator classes

--- calc.py
+++ calc.py
@@ -1,13 +1,100 @@
 import argparse
 
-# All valid chars of input expression
-ACCEPTED_CHARS = {
-    'op_unary': ['-'],
-    'op_binary': ['+', '-', '*', '/'],
-    'par': ['(', ')']
-}
 
-VALID_CHARS_SET = {ch for val_list in ACCEPTED_CHARS.values() for ch in val_list}
+class OperatorBase(object):
+
+    def __init__(self, token, precedence, operands=2, associativity='left'):
+        self.token = token
+        self.precedence = precedence
+        self.operands = operands
+        self.associativy = associativity
+
+    def __str__(self):
+        return "%s%d" % (self.token, self.operands)
+
+    def get_token(self):
+        return self.token
+
+    def get_precedence(self):
+        return self.precedence
+
+    def is_unary(self):
+        return self.operands == 1
+
+    def is_binary(self):
+        return self.operands == 2
+
+    def is_left_assoc(self):
+        return self.associativy == 'left'
+
+    def is_right_assoc(self):
+        return self.associativy == 'right'
+
+    def eval(self, *args):
+        assert len(args) == self.operands, "Bad number of operands for operator %s: expected %d, got %d" % (
+            self, self.operands, len(args))
+        self.do_eval(*args)
+
+    def do_eval(self, *args):
+        """ Override this in Operator subclasses"""
+        raise NotImplementedError()
+
+
+class Plus(OperatorBase):
+
+    def __init__(self):
+        super(Plus, self).__init__('+', 1)
+
+    def do_eval(self, *args):
+        return args[0] + args[1]
+
+
+class Minus(OperatorBase):
+
+    def __init__(self):
+        super(Minus, self).__init__('-', 1)
+
+    def do_eval(self, *args):
+        return args[0] - args[1]
+
+
+class Multiply(OperatorBase):
+
+    def __init__(self):
+        super(Multiply, self).__init__('*', 3)
+
+    def do_eval(self, *args):
+        return args[0] * args[1]
+
+
+class Divide(OperatorBase):
+
+    def __init__(self):
+        super(Divide, self).__init__('/', 3)
+
+    def do_eval(self, *args):
+        return args[0] / args[1]
+
+
+class UnaryMinus(OperatorBase):
+
+    def __init__(self):
+        super(UnaryMinus, self).__init__('-', 2, 1)
+
+    def do_eval(self, *args):
+        return args[0] * (-1)
+
+
+OPERATORS = [
+    Plus(), Minus(), Multiply(), Divide(), UnaryMinus()
+]
+
+BINARY_OPS = [op.get_token() for op in OPERATORS if op.is_binary()]
+UNARY_OPS = [op.get_token() for op in OPERATORS if op.is_unary()]
+
+VALID_TOKENS_SET = {op.get_token() for op in OPERATORS}
+VALID_TOKENS_SET.add('(')
+VALID_TOKENS_SET.add(')')
 
 
 class InvalidTokenError(Exception):
@@ -29,7 +116,7 @@ def tokenize(expr):
     result_list = []
     start = 0
     for index, char in enumerate(expr):
-        if char in VALID_CHARS_SET:
+        if char in VALID_TOKENS_SET:
             curr_token = (expr[start:index]).strip()  # strip() to avoid pushing blank tokens
             if len(curr_token) > 0:
                 result_list.extend(curr_token.split())  # split() because of possible "inner" blank spaces
@@ -60,7 +147,7 @@ def is_value(token):
 
 
 def validate_token(token):
-    if is_value(token) or token in VALID_CHARS_SET:
+    if is_value(token) or token in VALID_TOKENS_SET:
         return token
     raise InvalidTokenError(token)
 
@@ -83,14 +170,15 @@ class Recognizer(object):
     def _consume(self):
         self.cursor += 1  # move cursor up
 
-    def _error(self):
-        raise InvalidExpressionError()
+    def _error(self, msg=None):
+        raise InvalidExpressionError(msg)
 
     def _expect(self, token):
-        if self._next() == token:
+        try:
+            assert self._next() == token
             self._consume()
-        else:
-            self._error()
+        except AssertionError:
+            self._error("Expected %s, got %s" % (token, self._next()))
 
     def recognize(self):
         self._e()
@@ -98,7 +186,7 @@ class Recognizer(object):
 
     def _e(self):
         self._p()
-        while self._next() in ACCEPTED_CHARS['op_binary']:
+        while self._next() in BINARY_OPS:
             self._consume()
             self._p()
 
@@ -109,7 +197,7 @@ class Recognizer(object):
             self._consume()
             self._e()
             self._expect(')')
-        elif self._next() in ACCEPTED_CHARS['op_unary']:
+        elif self._next() in UNARY_OPS:
             self._consume()
             self._p()
         else:
